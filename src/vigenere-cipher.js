c5066fb5099264711ef619a69a056e1340f0d0e4
Kiselevich Stanislav <kiselevich.stasik@gmail.com>
2022-05-02 18:09:30 +0300

    feat: add vigenere

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,65 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class VigenereCipheringMachine {
+  constructor(direct = true) {
+    this.direct = direct;
+    this.alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    
+    
+    let arr = []
+    let res = ''
+    if(!message || !key){
+      throw new Error("Incorrect arguments!");
+    }
+    message = message.toUpperCase()
+    key = key.toUpperCase()
+    
+    
+    for(let i = 0, j = 0; i < message.length; i++){
+      if(j == key.length) j = 0
+      if(this.alph.indexOf(message[i]) == -1){
+        res += message[i]
+      }
+      else {
+        let pos = (this.alph.indexOf(message[i]) + this.alph.indexOf(key[j])) % 26
+        console.log(pos)
+        res += this.alph[pos]
+        j++
+      }
+      
+    }
+    
+    return this.direct ? res : res.split("").reverse().join("");
+  }
+  decrypt(message, key) {
+    let arr = []
+    let res = ''
+    if(!message || !key){
+      throw new Error("Incorrect arguments!");
+    }
+    message = message.toUpperCase()
+    key = key.toUpperCase()
+    
+    
+    for(let i = 0, j = 0; i < message.length; i++){
+      if(j == key.length) j = 0
+      if(this.alph.indexOf(message[i]) == -1){
+        res += message[i]
+      }
+      else {
+        let pos = (this.alph.indexOf(message[i]) - this.alph.indexOf(key[j])) % 26
+        if(pos < 0) pos += 26
+        console.log(pos)
+        res += this.alph[pos]
+        j++
+      }
+      
+    }
+    
+    return this.direct ? res : res.split("").reverse().join("");
   }
 }
 
